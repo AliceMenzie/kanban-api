8dacec3948c5b5584908d57eb3052f6951df573f
Alice Menzie <Alice.Menzie@gmail.com>
2021-08-11 13:55:15 +1000

    created graphql mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,9 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_task_mutation, mutation: Mutations::CreateTaskMutation
+    field :create_status_mutation, mutation: Mutations::CreateStatusMutation
+    field :create_board_mutation, mutation: Mutations::CreateBoardMutation
+    field :create_user_mutation, mutation: Mutations::CreateUserMutation
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
