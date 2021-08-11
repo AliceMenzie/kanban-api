f4a48e84b8e3ad1bbfaf49f6cd070b93efebe7b9
Alice Menzie <Alice.Menzie@gmail.com>
2021-08-11 17:51:13 +1000

    skip session for devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-  devise_for :users
+  devise_for :users, skip: :sessions
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
   end
