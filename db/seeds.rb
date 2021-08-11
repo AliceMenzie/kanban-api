fe65759b25ec091e2d9cafc47d8368e49887085e
Alice Menzie <Alice.Menzie@gmail.com>
2021-08-11 21:13:01 +1000

    created new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying old records"
+Task.destroy_all
+Status.destroy_all
+Board.destroy_all
+User.destroy_all
+
+puts "Seeding started"
+puts "Creating DB"
+
+user = User.create(username: "User1",
+                   email: "User1@test.com",
+                   password: "password")
+
+board = user.boards.create(
+  project: "project1",
+)
+
+status = board.statuses.create(
+  column: "TODO",
+)
+
+task = status.tasks.create(
+
+  title: "create user stories",
+  description: "lorem ipsum dolor sit amet",
+)
+
+task = status.tasks.create(
+
+  title: "make ERD",
+  description: "lorem ipsum dolor sit amet",
+)
+
+task = status.tasks.create(
+
+  title: "wireframes",
+  description: "lorem ipsum dolor sit amet",
+)
+
+status = board.statuses.create(
+  column: "DOING",
+)
+status = board.statuses.create(
+  column: "DONE",
+)
+
+puts "Seeding complete"
