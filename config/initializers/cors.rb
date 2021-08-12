c85c30855787a55fdfacb22769f541232c4bfb04
Alice Menzie <Alice.Menzie@gmail.com>
2021-08-12 18:08:40 +1000

    changed cors access

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com', 'http://localhost:8080'
+    origins "example.com", "http://localhost:8000"
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
